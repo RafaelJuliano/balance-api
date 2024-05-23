4ecd292e3b24a4dbd98d0168a5f86e2c71a1ff08
RafaelJuliano <rafael.j.ferreira@hotmail.com>
2024-05-22 22:15:06 -0300

    DynamoDB connect

--- serverless.ts
+++ serverless.ts
@@ -18,11 +18,7 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
-    iam: {
-      role: {
-        statements: [...dynamoDb.roles],
-      },
-    },
+    iamRoleStatements: [...dynamoDb.roles],
   },
   functions,
   resources: {
--- src/handlers/hello-world/handler.ts
+++ src/handlers/hello-world/handler.ts
@@ -1,9 +1,28 @@
 import { APIGatewayProxyEventV2 } from 'aws-lambda'
+import { GetItemCommand, DynamoDBClient } from '@aws-sdk/client-dynamodb'
 
 const handler = async (event: APIGatewayProxyEventV2) => {
+  const client = new DynamoDBClient({})
+
+  const command = new GetItemCommand({
+    TableName: process.env.HEELO_TABLE_NAME,
+    Key: {
+      id: { S: '1' },
+    },
+  })
+
+  const response = await client.send(command)
+
+  if (!response) {
+    return {
+      statusCode: 404,
+      body: 'Not Found',
+    }
+  }
+
   return {
     statusCode: 200,
-    body: 'Hello World from lambda!',
+    body: response?.Item?.name?.S,
   }
 }
 
