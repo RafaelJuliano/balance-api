057d64375f20905302c8c559e84d1bf0965da31d
RafaelJuliano <rafael.j.ferreira@hotmail.com>
2024-05-22 21:05:01 -0300

    Hello World test lambda

---
+++ serverless.ts
@@ -0,0 +1,38 @@
+import type { AWS } from '@serverless/typescript'
+import functions from './src/handlers'
+
+const serverlessConfiguration: AWS = {
+  service: 'balance-api',
+  frameworkVersion: '3',
+  plugins: ['serverless-esbuild', 'serverless-offline', 'serverless-offline-ssm'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs18.x',
+    stage: '${opt:stage, "local"}',
+    region: 'us-east-1',
+    environment: {
+      ENV: '${self:provider.stage}',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+  },
+  functions,
+  package: { individually: true },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['@aws-sdk/client-lambda'],
+      target: 'node18',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
+    'serverless-offline-ssm': {
+      stages: ['local'],
+    },
+  },
+}
+
+module.exports = serverlessConfiguration
