6e50381ee911eb82aed77052a0caee5e10b37f03
RafaelJuliano <rafael.j.ferreira@hotmail.com>
2024-05-23 18:16:54 -0300

    Remove SQS - Fix deploy

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,9 +1,7 @@
 import helloWorld from './hello-world'
 import putMessage from './put-message'
-import handleEvent from './handle-event'
 
 export default {
   'hello-world': helloWorld,
-  'handle-event': handleEvent,
   'put-message': putMessage,
 }
